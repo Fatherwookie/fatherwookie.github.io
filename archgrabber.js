3bbf459e7fb0ac388ceda7379ac0091a55b8ea19
Fatherwookie <125630079+Fatherwookie@users.noreply.github.com>
2023-03-14 14:18:18 -0500

    Update archgrabber.js

--- archgrabber.js
+++ archgrabber.js
@@ -1,57 +1,46 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
-import javax.swing.*;
+function displayImages(images) {
+  let index = 0;
+  const container = document.getElementById('image-container');
 
-public class ImageDisplay extends JPanel implements ActionListener {
-
-    private Timer timer;
-    private ImageIcon[] images;
-    private JLabel imageLabel;
-    private int currentIndex = 0;
-
-    public ImageDisplay() {
-        setPreferredSize(new Dimension(400, 400));
-        setBackground(Color.WHITE);
-        
-        // Load the images
-        images = new ImageIcon[] {
-            new ImageIcon("image1.jpg"),
-            new ImageIcon("image2.jpg"),
-            new ImageIcon("image3.jpg")
-        };
-        
-        // Create the image label
-        imageLabel = new JLabel();
-        add(imageLabel);
-        
-        // Create and start the timer
-        timer = new Timer(3000, this);
-        timer.start();
+  function resizeImage(img, image) {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const maxWidth = windowWidth - 50; // Allow some padding
+    const maxHeight = windowHeight - 50;
+    const newImg = new Image();
+    newImg.onload = () => {
+      let width = newImg.width;
+      let height = newImg.height;
+      if (width > maxWidth) {
+        const ratio = maxWidth / width;
+        width *= ratio;
+        height *= ratio;
+      }
+      if (height > maxHeight) {
+        const ratio = maxHeight / height;
+        width *= ratio;
+        height *= ratio;
+      }
+      img.width = width;
+      img.height = height;
+      img.src = image;
     }
+    newImg.src = image;
+  }
 
-    public void actionPerformed(ActionEvent e) {
-        currentIndex++;
-        if (currentIndex >= images.length) {
-            currentIndex = 0;
-        }
-        imageLabel.setIcon(images[currentIndex]);
+  function showNextImage() {
+    index++;
+    if (index >= images.length) {
+      index = 0;
     }
+    const img = document.createElement('img');
+    container.innerHTML = '';
+    container.appendChild(img);
+    resizeImage(img, images[index]);
+  }
 
-    private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Image Display");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new ImageDisplay());
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+  showNextImage();
+  setInterval(() => {
+    showNextImage();
+  }, 3000);
 }

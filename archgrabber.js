0edd96c955c39a2e0df3934e23a81450c253b433
Fatherwookie <125630079+Fatherwookie@users.noreply.github.com>
2023-03-14 14:19:13 -0500

    Update archgrabber.js

--- archgrabber.js
+++ archgrabber.js
@@ -1,3 +1,15 @@
+const subredditName = 'ArchitecturePorn';
+const numberOfPosts = 20;
+
+async function getTopImages() {
+  const response = await fetch(`https://www.reddit.com/r/${subredditName}/top.json?sort=top&t=day&limit=${numberOfPosts}`);
+  const json = await response.json();
+  const children = json.data.children;
+  const images = children.filter(child => child.data.post_hint === 'image').map(child => child.data.url);
+
+  displayImages(images);
+}
+
 function displayImages(images) {
   let index = 0;
   const container = document.getElementById('image-container');
@@ -44,3 +56,5 @@ function displayImages(images) {
     showNextImage();
   }, 3000);
 }
+
+getTopImages();

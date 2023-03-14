4f904f83d0f2a75a34ff62cc860c5f3585705e2b
Fatherwookie <125630079+Fatherwookie@users.noreply.github.com>
2023-03-14 14:14:52 -0500

    Update archgrabber.js

--- archgrabber.js
+++ archgrabber.js
@@ -1,48 +1,57 @@
-function displayImages(images) {
-  let index = 0;
-  const container = document.getElementById('image-container');
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
 
-  function resizeImage(img) {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const maxWidth = windowWidth - 50; // Allow some padding
-    const maxHeight = windowHeight - 50;
-    const image = new Image();
-    image.onload = () => {
-      let width = image.width;
-      let height = image.height;
-      if (width > maxWidth) {
-        const ratio = maxWidth / width;
-        width *= ratio;
-        height *= ratio;
-      }
-      if (height > maxHeight) {
-        const ratio = maxHeight / height;
-        width *= ratio;
-        height *= ratio;
-      }
-      img.width = width;
-      img.height = height;
-    }
-    image.src = img.src;
-  }
+public class ImageDisplay extends JPanel implements ActionListener {
 
-  function showNextImage() {
-    const img = document.createElement('img');
-    container.innerHTML = '';
-    container.appendChild(img);
-    img.onload = () => {
-      resizeImage(img);
-      setInterval(() => {
-        showNextImage();
-      }, 3000);
-    }
-    index++;
-    if (index >= images.length) {
-      index = 0;
-    }
-    img.src = images[index];
-  }
+    private Timer timer;
+    private ImageIcon[] images;
+    private JLabel imageLabel;
+    private int currentIndex = 0;
 
-  showNextImage();
+    public ImageDisplay() {
+        setPreferredSize(new Dimension(400, 400));
+        setBackground(Color.WHITE);
+        
+        // Load the images
+        images = new ImageIcon[] {
+            new ImageIcon("image1.jpg"),
+            new ImageIcon("image2.jpg"),
+            new ImageIcon("image3.jpg")
+        };
+        
+        // Create the image label
+        imageLabel = new JLabel();
+        add(imageLabel);
+        
+        // Create and start the timer
+        timer = new Timer(3000, this);
+        timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        imageLabel.setIcon(images[currentIndex]);
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Image Display");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ImageDisplay());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }

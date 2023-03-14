742dc5819e63739cc213fbf69171cf03e1f28ed7
Fatherwookie <125630079+Fatherwookie@users.noreply.github.com>
2023-03-14 12:45:13 -0500

    Update archgrabber.js

--- archgrabber.js
+++ archgrabber.js
@@ -1,47 +1,23 @@
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+const subredditName = 'ArchitecturePorn';
+const numberOfPosts = 10;
 
-public class RedditScraper {
+async function getTopImages() {
+  const response = await fetch(`https://www.reddit.com/r/${subredditName}/top.json?sort=top&t=day&limit=${numberOfPosts}`);
+  const json = await response.json();
+  const children = json.data.children;
+  const images = children.filter(child => child.data.post_hint === 'image').map(child => child.data.url);
 
-    public static void main(String[] args) {
-        String subredditName = "ArchitecturePorn";
-        int numberOfPosts = 10;
-
-        try {
-            URL url = new URL("https://www.reddit.com/r/" + subredditName + "/top.json?sort=top&t=day&limit=" + numberOfPosts);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            JSONObject jsonObject = new JSONObject(response.toString());
-            JSONObject dataObject = jsonObject.getJSONObject("data");
-            JSONArray childrenArray = dataObject.getJSONArray("children");
-
-            List<String> images = new ArrayList<>();
-            for (int i = 0; i < childrenArray.length(); i++) {
-                JSONObject childObject = childrenArray.getJSONObject(i);
-                JSONObject childDataObject = childObject.getJSONObject("data");
-                if (childDataObject.getString("post_hint").equals("image")) {
-                    images.add(childDataObject.getString("url"));
-                }
-            }
-
-            System.out.println(images);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  displayImages(images);
 }
+
+function displayImages(images) {
+  const container = document.getElementById('image-container');
+
+  images.forEach(image => {
+    const img = document.createElement('img');
+    img.src = image;
+    container.appendChild(img);
+  });
+}
+
+getTopImages();
